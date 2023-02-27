77659cc27f402b710a0ff197d6955e0feb475283
Bois <next.to.you@qq.com>
2023-02-27 23:57:05 +0800

    add uml.png update test

--- strategy_pattern/rpg/rpggame_test.go
+++ strategy_pattern/rpg/rpggame_test.go
@@ -1,23 +1,45 @@
 package rpg
 
 import (
+	"github.com/stretchr/testify/suite"
 	"testing"
 )
 
-func TestRPGGame(t *testing.T) {
-	king := King{Character{Name: "国王"}}
-	king.SetWeapon(Axe{Name: "血吼", ATK: 30})
-	king.fight()
+type RPGSuite struct {
+	suite.Suite
+}
 
-	queen := Queen{Character{Name: "王后"}}
-	queen.SetWeapon(BowAndArrow{Name: "轻语之弓", ATK: 15})
-	queen.fight()
+func TestRPGSuite(t *testing.T) {
+	suite.Run(t, new(RPGSuite))
+}
 
-	troll := Troll{Character{Name: "巨魔"}}
-	troll.SetWeapon(Knife{Name: "多兰剑", ATK: 20})
-	troll.fight()
+func (su *RPGSuite) TestRPGGame() {
+	su.Run("国王", func() {
+		king := King{Character{Name: "国王"}}
+		king.SetWeapon(Axe{Name: "血吼", ATK: 30})
+		king.fight()
+		su.Equal("挥舞<<血吼>>(斧)对敌人造成了30点伤害", king.weapon.UseWeapon())
+	})
 
-	knight := Knight{Character{Name: "骑士"}}
-	knight.SetWeapon(Sword{Name: "霜之哀伤", ATK: 25})
-	knight.fight()
+	su.Run("王后", func() {
+		queen := Queen{Character{Name: "王后"}}
+		queen.SetWeapon(BowAndArrow{Name: "轻语之弓", ATK: 15})
+		queen.fight()
+		su.Equal("使用<<轻语之弓>>(弓箭)对敌人造成了15点伤害", queen.weapon.UseWeapon())
+	})
+
+	su.Run("巨魔", func() {
+		troll := Troll{Character{Name: "巨魔"}}
+		troll.SetWeapon(Knife{Name: "多兰剑", ATK: 20})
+		troll.fight()
+		su.Equal("挥舞<<多兰剑>>(小刀)对敌人造成了20点伤害", troll.weapon.UseWeapon())
+	})
+
+	su.Run("骑士", func() {
+		knight := Knight{Character{Name: "骑士"}}
+		knight.SetWeapon(Sword{Name: "霜之哀伤", ATK: 25})
+		knight.fight()
+		su.Equal("挥舞<<霜之哀伤>>(剑)对敌人造成了25点伤害", knight.weapon.UseWeapon())
+	})
+
 }
--- strategy_pattern/rpg2/rpggame_test.go
+++ strategy_pattern/rpg2/rpggame_test.go
@@ -1,25 +1,39 @@
 package rpg2
 
-import "testing"
+import (
+	"github.com/stretchr/testify/suite"
+	"testing"
+)
 
 type Character struct {
 	Weapon WeaponBehavior
 }
 
-func TestRPGGame(t *testing.T) {
-	c := Character{Weapon: &Sword{Name: "霜之哀伤", ATK: 999}}
-	c.Weapon.Display()
-	c.Weapon.Attack()
+type Rpg2Suite struct {
+	suite.Suite
+}
 
-	c.Weapon = &Sword{Name: "火之高兴", ATK: 888}
-	c.Weapon.Display()
-	c.Weapon.Attack()
+func TestRpg2Suite(t *testing.T) {
+	suite.Run(t, new(Rpg2Suite))
+}
 
-	c.Weapon = &Staff{Name: "鸡腿杖", MagicType: "火焰", ATK: 888}
-	c.Weapon.Display()
-	c.Weapon.Attack()
+func (su *Rpg2Suite) TestRPGGame() {
+	su.Run("剑", func() {
+		c := Character{Weapon: &Sword{Name: "霜之哀伤", ATK: 999}}
+		c.Weapon.Display()
+		c.Weapon.Attack()
 
-	c.Weapon = &Staff{Name: "远古法杖", MagicType: "砂石", ATK: 888}
-	c.Weapon.Display()
-	c.Weapon.Attack()
+		c.Weapon = &Sword{Name: "火之高兴", ATK: 888}
+		c.Weapon.Display()
+		c.Weapon.Attack()
+
+		c.Weapon = &Staff{Name: "鸡腿杖", MagicType: "火焰", ATK: 888}
+		c.Weapon.Display()
+		c.Weapon.Attack()
+
+		c.Weapon = &Staff{Name: "远古法杖", MagicType: "砂石", ATK: 888}
+		c.Weapon.Display()
+		c.Weapon.Attack()
+	})
+
 }
